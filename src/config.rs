ce301c31b538f53e054b7fcb312c3a4da41315f7
Julio Daniel Reyes <nemesiscodex@gmail.com>
2021-01-04 13:01:05 -0300

    Format tracing logs as json bunyan

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,17 @@
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::prelude::*;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 
 pub fn configure_tracing() {
     // Get filter config from environment RUST_LOG
     let env = EnvFilter::from_default_env();
 
-    let fmt = tracing_subscriber::fmt::layer();
+    let formatting_layer = 
+        BunyanFormattingLayer::new("fibonacci".into(), std::io::stdout);
 
     tracing_subscriber::registry()
         .with(env)
-        .with(fmt)
+        // .with(JsonStorageLayer)
+        .with(formatting_layer)
         .init();
 }
